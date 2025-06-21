ae56976badddef77f2781e5f509e6e1af352202e
Charlie's Boyfriend <166959314+ReinhardTristanHeydrich@users.noreply.github.com>
2025-06-21 11:22:05 -0300

    Removed Steam verification.

--- crates/launcher/src/steam.rs
+++ crates/launcher/src/steam.rs
@@ -13,23 +13,6 @@ use windows::{
 use crate::LauncherResult;
 
 #[instrument(skip_all, err)]
-pub fn require_steam<P: AsRef<Path>>(game_binary: P) -> LauncherResult<()> {
-    let steam_dll = game_binary.as_ref().with_file_name("steam_api64.dll");
-
-    let handle = unsafe { LoadLibraryW(&HSTRING::from(&*steam_dll))? };
-
-    let steam_api_init = unsafe {
-        mem::transmute::<_, extern "system" fn() -> bool>(
-            GetProcAddress(handle, s!("SteamAPI_Init"))
-                .ok_or_eyre("SteamAPI_Init export not found")?,
-        )
-    };
-
-    let result = steam_api_init()
-        .then_some(())
-        .ok_or_eyre("Steam is required to run this game");
-
-    unsafe { FreeLibrary(handle)? };
-
-    result
+pub fn require_steam<P: AsRef<Path>>(_game_binary: P) -> LauncherResult<()> {
+    Ok(())
 }
